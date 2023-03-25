d512d17f6ae742c91fcf1f7024fa793f414a4a9b
Adriano D'Alonzo <adrianodalonzo@gmail.com>
2023-03-24 22:26:55 -0400

    Added search skeleton

---
+++ GamingGroupFinder/Search.cs
@@ -0,0 +1,15 @@
+namespace GamingGroupFinder {
+    public class Search {
+        public List<Event> FindEvent(Game game) {
+            return null;
+        }
+
+        public List<Event> FindEvent(Platform platform) {
+            return null;
+        }
+
+        public List<Event> FindEvent(string rank) {
+            return null;
+        }
+    }
+}
