29dc1c13e5408b7cf3a34b30a87d067b2a5a77bf
Vincent Keedwell <vincent.keedwell@dawsoncollege.qc.ca>
2023-03-27 13:38:19 -0400

    Added unit tests for Game

---
+++ GamingGroupFinderTests/GameTests.cs
@@ -0,0 +1,21 @@
+using GamingGroupFinder;
+
+namespace GamingGroupFinderTests {
+    [TestClass]
+    public class GameTests {
+        public static List<Platform> CreateListPlatforms() {
+            List<Platform> platforms = new List<Platform>();
+            platforms.Add(new Platform("PC"));
+            platforms.Add(new Platform("XBOX"));
+
+            return platforms;
+        }
+
+        [TestMethod]
+        [ExpectedException (typeof(ArgumentNullException))]
+        public void TestConstructor_ThrowsWhenGivenNull() {
+            List<Platform> platforms = CreateListPlatforms();
+            Game valorant = new Game(null, platforms, "GOLD");
+        }
+    }
+}
