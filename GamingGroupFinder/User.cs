2ad0cd33b2cbc3cd25cf2d466a06716237453990
adridalo <adriano.dalonzo@dawsoncollege.qc.ca>
2023-03-28 15:23:12 -0400

    added a contact list for a user and a method to add a user to user's contact list

--- GamingGroupFinder/User.cs
+++ GamingGroupFinder/User.cs
@@ -2,15 +2,21 @@ namespace GamingGroupFinder {
     public class User {
         private string username {get;}
         private string password;
+        private List<User> contactList;
 
-        public User(string username, string password) {
+        public User(string username, string password, List<User> contactList) {
             this.username = username;
             this.password = password;
+            this.contactList = contactList;
         }
 
         public bool VerifyUser(string username, string password) {
             return false;
         }   
+
+        public void AddToContacts(string userName) {
+            // add checking for seeing if user exists in database
+        }
 
         public void ChangePassword(string newPassword) {
             this.password = newPassword;
