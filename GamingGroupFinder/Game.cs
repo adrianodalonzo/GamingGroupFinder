843b5029cedefd0884f943ec675029d3e44f6b78
adridalo <adriano.dalonzo@dawsoncollege.qc.ca>
2023-03-28 15:08:09 -0400

    Made rank into a tuple of 2 lists

--- GamingGroupFinder/Game.cs
+++ GamingGroupFinder/Game.cs
@@ -2,9 +2,10 @@ namespace GamingGroupFinder {
     public class Game {
         private string game {get;}
         private List<string> availablePlatforms;
-        private List<string> rank;
+        // key 1: rank name. key 2: rank value
+        private Tuple<List<string>, List<int>> rank;
 
-        public Game(string game, List<string> platforms, List<string> rank) {
+        public Game(string game, List<string> platforms, Tuple<List<string>, List<int>> rank) {
             this.game = game;
             this.availablePlatforms = platforms;
             this.rank = rank;
