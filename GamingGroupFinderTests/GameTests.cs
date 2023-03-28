032a99fb3018447b11ed5847c8b59aa2a5b37d96
Vincent Keedwell <vincent.keedwell@dawsoncollege.qc.ca>
2023-03-28 19:14:33 -0400

    Fixed GameTests

--- GamingGroupFinderTests/GameTests.cs
+++ GamingGroupFinderTests/GameTests.cs
@@ -3,19 +3,33 @@ using GamingGroupFinder;
 namespace GamingGroupFinderTests {
     [TestClass]
     public class GameTests {
-        public static List<Platform> CreateListPlatforms() {
-            List<Platform> platforms = new List<Platform>();
-            platforms.Add(new Platform("PC"));
-            platforms.Add(new Platform("XBOX"));
+        public static List<string> CreateListPlatforms() {
+            List<string> platforms = new List<string>();
+            platforms.Add("PC");
+            platforms.Add("NINTENDO SWITCH");
 
             return platforms;
+        }
+
+        public static Tuple<List<string>, List<int>> GameSplatoonRanks() {
+            List<string> splatoonRanks = new List<string>();
+            splatoonRanks.Add("A-");
+            splatoonRanks.Add("A");
+            splatoonRanks.Add("A+");
+
+            List<int> splatoonRankValues = new List<int>();
+            splatoonRankValues.Add(1);
+            splatoonRankValues.Add(2);
+            splatoonRankValues.Add(3);
+
+            return new Tuple<List<string>, List<int>>(splatoonRanks, splatoonRankValues);
         }
 
         [TestMethod]
         [ExpectedException (typeof(ArgumentNullException))]
         public void TestConstructor_ThrowsWhenGivenNull() {
-            List<Platform> platforms = CreateListPlatforms();
-            Game valorant = new Game(null, platforms, "GOLD");
+            List<string> platforms = CreateListPlatforms();
+            Game splatoon = new Game(null, platforms, GameSplatoonRanks());
             Assert.Fail();
         }
     }
