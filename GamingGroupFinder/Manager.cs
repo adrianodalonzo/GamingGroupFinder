6c20f308f4e5e54af4fe7d31703d1ea6378479ec
adridalo <adriano.dalonzo@dawsoncollege.qc.ca>
2023-03-28 15:27:32 -0400

    added more comments as to tasks to complete

--- GamingGroupFinder/Manager.cs
+++ GamingGroupFinder/Manager.cs
@@ -3,7 +3,7 @@ namespace GamingGroupFinder {
 
         // methods:
         // check if they can join an event
-        // 
+        // check if a user can view messages by either them sending a message or recieving one
         private User loggedInUser;
 
         public void SetLoggedInUser(User user) {
