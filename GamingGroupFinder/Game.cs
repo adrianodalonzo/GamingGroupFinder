f15f229af07c0687c47541a80a4243fab38e7bb6
Charles Alexandre Bouchard <charles_ab18@Charless-MacBook-Air.local>
2023-03-30 15:39:20 -0400

    Added validation to Game

--- GamingGroupFinder/Game.cs
+++ GamingGroupFinder/Game.cs
@@ -1,14 +1,41 @@
 namespace GamingGroupFinder {
     public class Game {
-        private string game {get;}
-        private List<string> availablePlatforms;
+        public string Name{
+            get{ return Name; }
+            set {
+                if (string.IsNullOrEmpty(value)){
+                    throw new ArgumentNullException("Name is null");
+                }
+                this.Name = value;
+            }
+        }
+        public List<string> AvailablePlatforms{
+            get{ return AvailablePlatforms; }
+            set {
+                if (value == null){
+                    throw new ArgumentNullException("AvailablePlatforms is null");
+                }
+                this.AvailablePlatforms = value;
+            }
+        }
         // key 1: rank name. key 2: rank value
-        private Tuple<List<string>, List<int>> rank;
+        public Tuple<List<string>, List<int>> Rank{
+            get{ return Rank; }
+            set {
+                if (value == null){
+                    throw new ArgumentNullException("Rank is null");
+                }
+                this.Rank = value;
+            }
+        }
 
-        public Game(string game, List<string> platforms, Tuple<List<string>, List<int>> rank) {
-            this.game = game;
-            this.availablePlatforms = platforms;
-            this.rank = rank;
+        public Game(string name, List<string> platforms, Tuple<List<string>, List<int>> rank) {
+            if (name == null || platforms == null || rank == null){
+                throw new ArgumentNullException("one or multiple of the input is null");
+            }
+            this.Name = name;
+            this.AvailablePlatforms = platforms;
+            this.Rank = rank;
         }
     }
 }
