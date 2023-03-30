3d0b95342adf29c8239c91906174c85761f2ebdb
Charles Alexandre Bouchard <charles_ab18@Charless-MacBook-Air.local>
2023-03-30 15:39:29 -0400

    Added validation to Manager

--- GamingGroupFinder/Manager.cs
+++ GamingGroupFinder/Manager.cs
@@ -4,10 +4,21 @@ namespace GamingGroupFinder {
         // methods:
         // check if they can join an event
         // check if a user can view messages by either them sending a message or recieving one
-        private User loggedInUser;
+        public User LoggedInUser{
+            get{ return LoggedInUser; }
+            set {
+                if (value == null){
+                    throw new ArgumentNullException("LoggedInUser is null");
+                }
+                this.LoggedInUser = value;
+            }
+        }
 
         public void SetLoggedInUser(User user) {
-            this.loggedInUser = user;
+            if (user == null){
+                throw new ArgumentNullException("user is null");
+            }
+            this.LoggedInUser = user;
         }
     }
 }
