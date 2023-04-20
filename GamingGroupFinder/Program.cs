0b132e332c9d5f9ef54debd7a5e006f5168af889
adridalo <adriano.dalonzo@dawsoncollege.qc.ca>
2023-04-20 15:38:58 -0400

    added test to see if adding a user and profile worked

--- GamingGroupFinder/Program.cs
+++ GamingGroupFinder/Program.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine("Welcome to GamingGroupFinder!");
+﻿using GamingGroupFinderDatabase;
+
+Console.WriteLine("Welcome to GamingGroupFinder!");
+
+ApplicationContext db = new ApplicationContext();
+User user = new User();
+user.Username = "Test";
+user.UserId = 1;
+user.Salt = "134jidkfgjdf";
+user.Password = "jhfsd76y79n2b3b5bbjn`l;";
+user.EventsAttending = new List<Event>();
+user.EventsOwned = new List<Event>();
+
+Profile profile = new Profile();
+profile.Age = 19;
+profile.Bio = "bio";
+profile.Name = "Test";
+profile.ProfileId = 1;
+profile.ProfilePicture = "link";
+profile.Pronouns = "they/them";
+profile.User = user;
+
+user.Profile = profile;
+db.Add(user);
+db.SaveChanges();
+Console.WriteLine("User added");
