f57f8ac2e299d663ae1d35292319c85aa97db05e
PepeLaugh <charles-alexandre.bouchard@dawsoncollege.qc.ca>
2023-04-20 20:08:52 -0400

    changed the name of rank list to ranks in Game object

--- GamingGroupFinder/Game.cs
+++ GamingGroupFinder/Game.cs
@@ -21,24 +21,24 @@ namespace GamingGroupFinder {
             }
         }
         // key 1: rank name. key 2: rank value
-        private List<Rank> _rank;
-        public List<Rank> Rank{
-            get{ return _rank; }
+        private List<Rank> _ranks;
+        public List<Rank> Ranks{
+            get{ return _ranks; }
             set {
                 if (value == null){
                     throw new ArgumentNullException("Rank is null");
                 }
-                this._rank = value;
+                this._ranks = value;
             }
         }
 
-        public Game(string name, List<string> platforms, List<Rank> rank) {
-            if (name == null || platforms == null || rank == null){
+        public Game(string name, List<string> platforms, List<Rank> ranks) {
+            if (name == null || platforms == null || ranks == null){
                 throw new ArgumentNullException("one or multiple of the input is null");
             }
             this.Name = name;
             this.AvailablePlatforms = platforms;
-            this.Rank = rank;
+            this.Ranks = ranks;
         }
         private Game(){
         }
