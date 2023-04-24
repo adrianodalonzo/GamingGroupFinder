d7ae7155bd062a2b9349fd26eedec14eb88b92ac
Adriano D'Alonzo <adrianodalonzo@gmail.com>
2023-04-24 17:23:07 -0400

    created validation for user manager

---
+++ GamingGroupFinder/Migrations/20230424204142_GameRanksBridgingTable.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _410project.Migrations
+{
+    /// <inheritdoc />
+    public partial class GameRanksBridgingTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GameDBRankDB");
+
+            migrationBuilder.CreateTable(
+                name: "GamesDBRanksDB",
+                columns: table => new
+                {
+                    GameDBId = table.Column<int>(type: "NUMBER(10)", nullable: false),
+                    RankDBId = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.ForeignKey(
+                        name: "FK_GamesDBRanksDB_GamesDB_GameDBId",
+                        column: x => x.GameDBId,
+                        principalTable: "GamesDB",
+                        principalColumn: "GameDBId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_GamesDBRanksDB_RanksDB_RankDBId",
+                        column: x => x.RankDBId,
+                        principalTable: "RanksDB",
+                        principalColumn: "RankDBId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GamesDBRanksDB_GameDBId",
+                table: "GamesDBRanksDB",
+                column: "GameDBId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GamesDBRanksDB_RankDBId",
+                table: "GamesDBRanksDB",
+                column: "RankDBId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GamesDBRanksDB");
+
+            migrationBuilder.CreateTable(
+                name: "GameDBRankDB",
+                columns: table => new
+                {
+                    GamesGameDBId = table.Column<int>(type: "NUMBER(10)", nullable: false),
+                    RanksRankDBId = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GameDBRankDB", x => new { x.GamesGameDBId, x.RanksRankDBId });
+                    table.ForeignKey(
+                        name: "FK_GameDBRankDB_GamesDB_GamesGameDBId",
+                        column: x => x.GamesGameDBId,
+                        principalTable: "GamesDB",
+                        principalColumn: "GameDBId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_GameDBRankDB_RanksDB_RanksRankDBId",
+                        column: x => x.RanksRankDBId,
+                        principalTable: "RanksDB",
+                        principalColumn: "RankDBId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GameDBRankDB_RanksRankDBId",
+                table: "GameDBRankDB",
+                column: "RanksRankDBId");
+        }
+    }
+}
--- GamingGroupFinder/managers/UserManager.cs
+++ GamingGroupFinder/managers/UserManager.cs
@@ -7,21 +7,33 @@ namespace GamingGroupFinder {
         // check if they can join an event
         // check if a user can view messages by either them sending a message or recieving one
 
-        private ApplicationContext db = new ApplicationContext();
+        private static ApplicationContext db = new ApplicationContext();
         private User _loggedInUser;
         public User LoggedInUser{
             get{ return _loggedInUser; }
-            set {
-                if (value == null){
-                    throw new ArgumentNullException("LoggedInUser is null");
+            set { this._loggedInUser = value; }
+        }
+
+        public static List<UserDB> GetListOfUsers() {
+            List<UserDB> users = db.UsersDB.ToList();
+            return users;
+        }
+
+        private static bool UserExists(User user, List<UserDB> users) {
+            foreach(UserDB testUser in users) {
+                if(testUser.Username.Equals(user.Username)) {
+                    return true;
                 }
-                this._loggedInUser = value;
             }
+            return false;
         }
 
         // this is probably just going to create a new user and add it to the database
         public void CreateUser(User u) {
             // add checking for an existing user
+            if(UserExists(u, GetListOfUsers())) {
+                throw new Exception("User already exists!");
+            }
             UserDB userEntity = new UserDB(u.Username, u.Password, u.Salt, null);
             db.Add(userEntity);
             db.SaveChanges();
@@ -31,23 +43,41 @@ namespace GamingGroupFinder {
             if (user == null){
                 throw new ArgumentNullException("user is null");
             }
-            this.LoggedInUser = user;
+            try {
+                if(LoggedInUser.Username.Equals(user.Username)) {
+                    throw new Exception("This user is already logged in!");
+                }
+            } catch (NullReferenceException) {
+                // this is fine, it just means that no one is logged in
+                this.LoggedInUser = user;
+            }
         }
 
         public void LogOutUser() {
+            if(LoggedInUser == null) {
+                throw new Exception("User is already logged out/never logged in!");
+            }
             this.LoggedInUser = null;
         }
 
         public void ChangePassword(string password) {
             UserDB testUser = (from user in db.UsersDB where user.Username.Equals(LoggedInUser.Username) select user).Single();
-            testUser.Password = password;
-            db.SaveChanges();
+            if(LoggedInUser.Username.Equals(testUser.Username)) {
+                testUser.Password = password;
+                db.SaveChanges();
+            } else {
+                throw new Exception("Only the user of the requested account may change their password!");
+            }
         }
 
         public void DeleteAccount() {
             UserDB testUser = (from user in db.UsersDB where user.Username.Equals(LoggedInUser.Username) select user).Single();
-            db.UsersDB.Remove(testUser);
-            db.SaveChanges();
+            if(LoggedInUser.Username.Equals(testUser.Username)) {
+                db.UsersDB.Remove(testUser);
+                db.SaveChanges();
+            } else {
+                throw new Exception("Only the user of the requested account may delete their account!");
+            }
         }
     }
 }
