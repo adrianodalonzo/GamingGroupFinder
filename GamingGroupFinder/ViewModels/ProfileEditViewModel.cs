e346d6bd8b94ce85e0c7046f9bc5fc962c5784ac
adridalo <adriano.dalonzo@dawsoncollege.qc.ca>
2023-05-04 15:45:15 -0400

    finished profile edit and display

--- GamingGroupFinder/ViewModels/ProfileEditViewModel.cs
+++ GamingGroupFinder/ViewModels/ProfileEditViewModel.cs
@@ -10,8 +10,26 @@ namespace GamingGroupFinderGUI.ViewModels
         
         public static ProfileDB Profile {get; set;}
         public ReactiveCommand<Unit, Unit> Ok { get; }
-        public List<string> GameNames { get; } = GetGameNames();
-        public List<string> PlatformNames { get; } = GetPlatformNames();
+        public static List<string> GameNames { get; } = GetGameNames();
+        public static List<string> PlatformNames { get; } = GetPlatformNames();
+        public string _selectedGame;
+        public string SelectedGame
+        {
+            get => _selectedGame;
+            private set => this.RaiseAndSetIfChanged(ref _selectedGame, value);
+        }
+        public string _selectedInterest;
+        public string SelectedInterest
+        {
+            get => _selectedInterest;
+            private set => this.RaiseAndSetIfChanged(ref _selectedInterest, value);
+        }
+        public string _selectedPlatform;
+        public string SelectedPlatform
+        {
+            get => _selectedPlatform;
+            private set => this.RaiseAndSetIfChanged(ref _selectedPlatform, value);
+        }
         public ProfileEditViewModel(ProfileDB p)
         {
             Profile = p;
@@ -28,7 +46,22 @@ namespace GamingGroupFinderGUI.ViewModels
                 return null;
             }
             Profile.Games.Add(game);
+            // GameNames.Remove(gameName);
             return game;
+        }
+
+        public static PlatformDB AddPlatform(string platformName) {
+            var platform = (from p in PlatformManager.GetListOfPlatforms() where p.PlatformName.Equals(platformName) select p).FirstOrDefault();
+            if(platform is null) {
+                return null;
+            }
+            Profile.Platforms.Add(platform);
+            // PlatformNames.Remove(platformName);
+            return platform;
+        }
+
+        public static void AddInterest(string interest) {
+            Profile.Interests.Add(new InterestDB(interest));
         }
 
         public static List<string> GetGameNames() {
