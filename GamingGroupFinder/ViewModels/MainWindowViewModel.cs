948a413a093bfdecec08e1e53cb793ba198859ef
Adriano D'Alonzo <adrianodalonzo@gmail.com>
2023-05-07 19:35:34 -0400

    added icons and code to generate nav bar for main view model

--- GamingGroupFinder/ViewModels/MainWindowViewModel.cs
+++ GamingGroupFinder/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using GamingGroupFinder;
+﻿using System.Reactive;
+using GamingGroupFinder;
 using GamingGroupFinderGUI.Models;
 using ReactiveUI;
 
@@ -7,6 +8,11 @@ namespace GamingGroupFinderGUI.ViewModels;
 class MainWindowViewModel : ViewModelBase
     {
         ViewModelBase content;
+        private bool _visibleNavigation;
+        public bool VisibleNavigation {
+            get => _visibleNavigation;
+            private set => this.RaiseAndSetIfChanged(ref _visibleNavigation, value);
+        }
         UserDB? LoggedInUser;
 
         public ViewModelBase Content
@@ -15,12 +21,34 @@ class MainWindowViewModel : ViewModelBase
             private set => this.RaiseAndSetIfChanged(ref content, value);
         }
 
+        public ReactiveCommand<Unit, Unit> Profile { get; }
+        public ReactiveCommand<Unit, Unit> NewEvent { get; }
+        public ReactiveCommand<Unit, Unit> Search { get; }
+        public ReactiveCommand<Unit, Unit> Logout { get; }
 
-        public MainWindowViewModel()
+
+        public MainWindowViewModel() {
+            Profile = ReactiveCommand.Create(() => {ShowPersonalProfile();});
+            NewEvent =  ReactiveCommand.Create(() => {CreateEvent();});
+            Search  = ReactiveCommand.Create(() => {OpenSearch();});
+            Logout = ReactiveCommand.Create(() => {Login();});
+
+            Login();
+        }
+
+        private void ShowPersonalProfile()
         {
+            DisplayProfile(ProfileManager.GetProfile(LoggedInUser));
+        }
+
+        private void DisplayProfile(ProfileDB profile) {
+            Content = new ProfileDisplayViewModel(profile);
+        }
+
+        public void Login() {
+            VisibleNavigation = false;
 
             LogInViewModel vm = new LogInViewModel();
-
             vm.Login.Subscribe(x => {PrepareMainPage(vm.LoginUser());});
             vm.Register.Subscribe(x => {PrepareMainPage(vm.RegisterUser());});
             Content = vm;
@@ -28,11 +56,27 @@ class MainWindowViewModel : ViewModelBase
 
 
         public void PrepareMainPage(UserDB u) {
+            VisibleNavigation = true;
             if(u != null) {
                 LoggedInUser = u;
                 ProfileDB p = ProfileManager.GetProfile(LoggedInUser);
-                Content = new ProfileDisplayViewModel(p);
+                ShowPersonalProfile();
             }
+        }
+
+        private void OpenSearch()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CreateEvent()
+        {
+            // DisplayEvent(new EventDB());
+        }
+
+        private void DisplayEvent(EventDB e)
+        {
+            Content = new EventDisplayViewModel(e);
         }
        
 
