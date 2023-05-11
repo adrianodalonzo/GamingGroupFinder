0eb60d51a01917a8541a865bcf259db11d1b68e6
Charles Alexandre Bouchard <charles_ab18@Charless-MacBook-Air.local>
2023-05-11 15:39:11 -0400

    Added ProfilePicture from url functionality

--- GamingGroupFinder/ViewModels/ProfileDisplayViewModel.cs
+++ GamingGroupFinder/ViewModels/ProfileDisplayViewModel.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Net;
 using System.Reactive;
 using GamingGroupFinder;
 using GamingGroupFinderGUI.Models;
@@ -21,6 +22,50 @@ namespace GamingGroupFinderGUI.ViewModels
             if(UserManager.GetInstance().LoggedInUser.Username.Equals(p.User.Username)) {
                 Visible = true;
             }
+            ProfilePictureUrl = Profile.ProfilePicture;
+        }
+        private string profilePictureUrl;
+        public string ProfilePictureUrl
+        {
+            get => profilePictureUrl;
+            set {
+                this.RaiseAndSetIfChanged(ref profilePictureUrl, value);
+                DownloadImage(ProfilePictureUrl);
+                System.Diagnostics.Debug.WriteLine(ProfilePictureUrl);
+            }
+        }
+
+        private Avalonia.Media.Imaging.Bitmap profilePicture = null;
+        public Avalonia.Media.Imaging.Bitmap ProfilePicture
+        {
+            get => profilePicture;
+            set => this.RaiseAndSetIfChanged(ref profilePicture, value);
+        }
+        public void DownloadImage(string url)
+        {
+            using (WebClient client = new WebClient())
+            {
+                client.DownloadDataAsync(new Uri(url));
+                client.DownloadDataCompleted += DownloadComplete;
+            }
+        }
+        private void DownloadComplete(object sender, DownloadDataCompletedEventArgs e)
+        {
+            try
+            {
+                byte[] bytes = e.Result;
+
+                Stream stream = new MemoryStream(bytes);
+
+                var image = new Avalonia.Media.Imaging.Bitmap(stream);
+                ProfilePicture = image;
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex);
+                ProfilePicture = null; // Could not download...
+            }
+            
         }
 
     }
